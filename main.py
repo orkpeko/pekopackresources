18b943c0b22c9cdf0d59a5e6396f0c2d5310b544
Ryan <85147248+orkpeko@users.noreply.github.com>
2022-02-11 15:59:03 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import zipfile
 
 def rgb_hack(rgb):
     return "#%02x%02x%02x" % rgb
-
+print("Connecting to api.orkpeko.xyz...")
 # TK Window define
 window = Tk()
 window.geometry('853x480')
@@ -27,7 +27,7 @@ def api_rqt():
     return a
 
 
-def clickdownloadmods():
+def clickdownloadmodso():
     input("Press enter in the console to confirm, as this will delete everything in your mods folder.")
     folder = f"{diren.get()}\\mods"
     for filename in os.listdir(folder):
@@ -40,7 +40,26 @@ def clickdownloadmods():
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
     print('Sending request to cdn.orkpeko.xyz for the mod pack...')
-    r = requests.get('https://cdn.orkpeko.xyz/r/latestm.zip', allow_redirects=True)
+    r = requests.get('https://cdn.orkpeko.xyz/r/latestmo.zip', allow_redirects=True)
+    open(f"{diren.get()}\\mods\\modste.zip", 'wb').write(r.content)
+    with zipfile.ZipFile(f"{diren.get()}\\mods\\modste.zip", 'r') as zip_ref:
+        zip_ref.extractall(f"{diren.get()}\\mods",)
+    print("Done.")
+
+def clickdownloadmodss():
+    input("Press enter in the console to confirm, as this will delete everything in your mods folder.")
+    folder = f"{diren.get()}\\mods"
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    print('Sending request to cdn.orkpeko.xyz for the mod pack...')
+    r = requests.get('https://cdn.orkpeko.xyz/r/latestms.zip', allow_redirects=True)
     open(f"{diren.get()}\\mods\\modste.zip", 'wb').write(r.content)
     with zipfile.ZipFile(f"{diren.get()}\\mods\\modste.zip", 'r') as zip_ref:
         zip_ref.extractall(f"{diren.get()}\\mods",)
@@ -56,19 +75,22 @@ def clickdownloadtexture():
 diren = Entry(window, text="Download Modpack", width=70)
 diren.place(rely=0., relx=0.5, x=0, y=0, anchor=N)
 
-labeluu = Label(window, text=f"Updater Updates:\n{api_rqu()}.", width=30, fg="#FFFFFF", bg="#000000")
+labeluu = Label(window, text=f"Updater Updates:\n{api_rqu()}", width=300, fg="#FFFFFF", bg="#000000")
 labeluu.place(rely=0.05, relx=0.5, x=0, y=0, anchor=N)
 
-buttonm = Button(window, text="Download Modpack", width=30, command=clickdownloadmods)
-buttonm.place(rely=0.7, relx=0.3, x=0, y=0, anchor=N)
+buttonm = Button(window, text="Download Modpack (OF)", width=30, command=clickdownloadmodso)
+buttonm.place(rely=0.67, relx=0.3, x=0, y=0, anchor=N)
 
-labelmu = Label(window, text=f"Updates:\n{api_rqm()}.", width=30, fg="#FFFFFF", bg="#000000")
+buttonm = Button(window, text="Download Modpack (Sodium)", width=30, command=clickdownloadmodss)
+buttonm.place(rely=0.73, relx=0.3, x=0, y=0, anchor=N)
+
+labelmu = Label(window, text=f"Updates:\n{api_rqm()}", width=30, fg="#FFFFFF", bg="#000000")
 labelmu.place(rely=0.3, relx=0.3, x=0, y=0, anchor=N)
 
 buttont = Button(window, text="Download Texturepack", width=30, command=clickdownloadtexture)
 buttont.place(rely=0.7, relx=0.7, x=0, y=0, anchor=N)
 
-labeltu = Label(window, text=f"Updates:\n{api_rqt()}.", width=30, fg="#FFFFFF", bg="#000000")
+labeltu = Label(window, text=f"Updates:\n{api_rqt()}", width=30, fg="#FFFFFF", bg="#000000")
 labeltu.place(rely=0.3, relx=0.7, x=0, y=0, anchor=N)
 
 diren.delete(0, "end")
@@ -76,5 +98,7 @@ diren.insert(0, f"{os.environ['USERPROFILE']}\\AppData\\Roaming\\.minecraft")
 
 txtpro = Label(window, text="github.com/orkpeko")
 txtpro.place(rely=1, relx=1, x=0, y=0, anchor=SE)
+
+print("Connected")
 
 window.mainloop()
